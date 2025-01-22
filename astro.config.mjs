3081aea868dd74ab550b32910669d7e8d7d2f425
A. Soto <agsoto@protonmail.com>
2025-01-21 21:18:31 -0500

    Deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ import config from './src/site-config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://agsoto.github.io',
   // base: '/docs',
   trailingSlash: 'ignore',
 
