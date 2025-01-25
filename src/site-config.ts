1a84825a5a1df2804bd29a55d87aa3db1aa2124f
A. Soto <agsoto@protonmail.com>
2025-01-25 11:58:34 -0500

    Update github link

--- src/site-config.ts
+++ src/site-config.ts
@@ -65,7 +65,7 @@ export const theme: ThemeUserConfig = {
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: true,
     /** Optional details about the social media accounts for this site. */
-    social: { github: 'https://astro.build/chat' }
+    social: { github: 'https://github.com/agsoto' }
   },
 
   content: {
