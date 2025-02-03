d3c291d484dfe99580fb859ccc07c2759aed5215
A. Soto <agsoto@protonmail.com>
2025-02-02 20:16:11 -0500

    Final bilingual localization and site config

--- src/site-config.ts
+++ src/site-config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: 'Andrés Soto',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Know Yourself',
+  description: 'Andrés` Personal Website to Know Himself',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
