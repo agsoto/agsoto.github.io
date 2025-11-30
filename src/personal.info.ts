1ed55794cee55090a9d039a47447639319133918
A. Soto <agsoto@protonmail.com>
2025-11-30 16:04:04 -0500

    Customize with personal information

---
+++ src/personal.info.ts
@@ -0,0 +1,5 @@
+export const personalInfo: PersonalInfo = {
+    name: 'Andrés Soto',
+    email: 'agsoto@protonmail.com',
+    phone: '+573229068394'
+}
--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,9 @@ declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
 }
+
+interface PersonalInfo {
+    name: string;
+    email: string;
+    phone: string;
+}
